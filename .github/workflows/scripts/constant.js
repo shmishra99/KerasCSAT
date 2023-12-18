fc144140e14415e5b108f06b7d51b1174df7d7ba
Shivam Mishra <124146945+shmishra99@users.noreply.github.com>
2023-12-18 22:45:43 +0530

    ISSUEID_PARAM

--- .github/workflows/scripts/constant.js
+++ .github/workflows/scripts/constant.js
@@ -5,7 +5,7 @@ let CONSTANT_VALUES = {
             NO:'No',
             BASE_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSdHag0RVFS7UXzZkKcsFCKOcX8raCupKK9RHSlYxp5U8lSJbQ/viewform?',
             SATISFACTION_PARAM: 'entry.492125872=',
-            ISSUEID_PRAM: '&entry.243948740=',
+            ISSUEID_PARAM: '&entry.243948740=',
             MSG: 'Are you satisfied with the resolution of your issue?',
             CSAT_LABELS: ['type:bug', 'type:build/install', 'type:support', 'type:others', 'type:docs-bug', 'type:performance']
         }
