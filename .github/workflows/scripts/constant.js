0a144fd15cd84b8458727b523d431f67fad7ce7d
Shivam Mishra <124146945+shmishra99@users.noreply.github.com>
2023-12-18 22:37:39 +0530

    csalink change

--- .github/workflows/scripts/constant.js
+++ .github/workflows/scripts/constant.js
@@ -3,15 +3,12 @@ let CONSTENT_VALUES = {
         CSAT: {
             YES:'Yes',
             NO:'No',
-            BASE_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSfaP12TRhd9xSxjXZjcZFNXPG' +
-        'k4kc1-qMdv3gc6bEP90vY1ew/viewform?',
-            MEDIA_PIPE_BASE_URL: 'https://docs.google.com/forms/d/e/1FAIpQLScOLT8zeBHummIZFnfr9wqvxYzWD1DAypyvNia5WVIWtFANYg/viewform?',
-            SATISFACTION_PARAM: 'entry.85265664=',
-            ISSUEID_PRAM: '&entry.2137816233=',
+            BASE_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSdHag0RVFS7UXzZkKcsFCKOcX8raCupKK9RHSlYxp5U8lSJbQ/viewform?',
+            SATISFACTION_PARAM: 'entry.492125872=',
+            ISSUEID_PRAM: '&entry.243948740=',
             MSG: 'Are you satisfied with the resolution of your issue?',
             CSAT_LABELS: ['type:bug', 'type:build/install', 'type:support', 'type:others', 'type:docs-bug', 'type:performance']
         }
     }
-    
 }
 module.exports = CONSTENT_VALUES
