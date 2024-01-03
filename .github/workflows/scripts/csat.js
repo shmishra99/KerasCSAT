24b7460962a73f5c6a519d73d2a9c717b7cb1c15
Shivam Mishra <124146945+shmishra99@users.noreply.github.com>
2024-01-03 18:14:47 +0530

    Update csat.js

--- .github/workflows/scripts/csat.js
+++ .github/workflows/scripts/csat.js
@@ -6,6 +6,7 @@ post survey link in closed issue.
 */
 module.exports = async ({ github, context }) => {
     const issue = context.payload.issue.html_url;
+    console.log(CONSTANT_VALUES)
     let base_url = CONSTANT_VALUES.MODULE.CSAT.BASE_URL;
      //Loop over all ths label present in issue and check if specific label is present for survey link.
     for (const label of context.payload.issue.labels) {
